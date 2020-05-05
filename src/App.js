ec00ab14f248e39736eb5d98fc5bb35c89fdaafa
vinayak <gaikwadvinayak291@gmail.com>
2020-05-05 12:12:08 +0530

    joining backend

--- src/App.js
+++ src/App.js
@@ -23,9 +23,29 @@ class App extends Component {
             box: {},
             route: 'signin',
             isSignedIn: true,
+            user:{
+                id:'',
+                name:'',
+                email:'',
+                entries: 0,
+                joined: '',
+            }
 
         }
     }
+
+    loadUser= (data) =>{
+        this.setState({user:{
+            id:data.id,
+            name:data.name,
+            email:data.email,
+            entries: data.entries,
+            joined: data.joined,
+        }
+    }
+        )
+    }
+
 
     calculateFaceLocation = (data) => {
         const face = data.outputs[0].data.regions[0].region_info.bounding_box ;
@@ -44,7 +64,7 @@ class App extends Component {
     }
 
     displayFace = (box) => {
-        console.log(box);
+        
         this.setState({box: box});
     }
 
@@ -55,7 +75,22 @@ class App extends Component {
     onButtonSubmit = () => {
         this.setState({imageUrl: this.state.input});
         app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        .then(response => this.displayFace(this.calculateFaceLocation(response)))
+        .then(response => {
+            if(response){
+                fetch('http://localhost:3000/image', {
+                method: 'put',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                id:this.state.user.id
+            })
+            })
+                .then(response => response.json())
+                .then(count => {
+                    this.setState(Object.assign(this.state.user, {entries:count}))
+                })
+            }
+            this.displayFace(this.calculateFaceLocation(response))
+        })
         .catch(err => console.log(err));
     }
 
@@ -73,35 +108,36 @@ class App extends Component {
     render() {
 
         return (
-        <div className="App">
-         <Particles
-            className='particle' 
-              params={{
-                    particles: {
-                        number: {
-                            value:50,
-                            density: {
-                                enable: true,
-                                value_area: 500
-                            }
-                        }
-                    }
-                }}
+        // <div className="App">
+        //  <Particles
+        //     className='particle' 
+        //       params={{
+        //             particles: {
+        //                 number: {
+        //                     value:50,
+        //                     density: {
+        //                         enable: true,
+        //                         value_area: 500
+        //                     }
+        //                 }
+        //             }
+        //         }}
               
-            />
+        //     />
+        <div>
         <Navigation isSignedIn={this.state.isSignedIn} onRouteChange= {this.onRouteChange} />
         {
             (this.state.route === 'home')?
                 <div>
                     <Logo/>
-                    <Rank />
+                    <Rank name= {this.state.user.name} entries={this.state.user.entries}/>
                     <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
                     <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box}/>
                 </div>
            :(this.state.route === 'signin'?
-                <SignIn onRouteChange= {this.onRouteChange} />
+                <SignIn loadUser={this.loadUser} onRouteChange= {this.onRouteChange} />
             :
-                <Register onRouteChange= {this.onRouteChange} />
+                <Register loadUser={this.loadUser} onRouteChange= {this.onRouteChange} />
 
 
 
