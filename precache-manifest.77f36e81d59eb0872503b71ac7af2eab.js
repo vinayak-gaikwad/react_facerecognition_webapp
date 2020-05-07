38c0984d19ebac1a009800f7a24e89db1560261f
Gladiator <gaikwadvinayak291@gmail.com>
2020-05-07 11:47:27 +0530

    Updates

---
+++ precache-manifest.77f36e81d59eb0872503b71ac7af2eab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17fd4d81895e114939fffc0014c6b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80b621ee4bb07d3ee15c",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "08eb4e8d3ac33ffbb655",
+    "url": "/static/css/main.8fa9cd56.chunk.css"
+  },
+  {
+    "revision": "80b621ee4bb07d3ee15c",
+    "url": "/static/js/2.ab11a922.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab11a922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08eb4e8d3ac33ffbb655",
+    "url": "/static/js/main.0828230a.chunk.js"
+  },
+  {
+    "revision": "886d0df223d3918924dd",
+    "url": "/static/js/runtime-main.33111a8f.js"
+  },
+  {
+    "revision": "7899fe7d17cc3de54d44b8a357244ee5",
+    "url": "/static/media/Logo.7899fe7d.png"
+  }
+]);
